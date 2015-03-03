eb1bc80ddf027801b0592d44d967ef3559fd172d
srt32 <simon.taranto@gmail.com>
2015-03-03 13:58:46 -0800

    Adds unique constraint to memberships
    There are duplicate `membership` records in the database:
    
    ```
    SELECT distinct(user_id)
    FROM memberships
    GROUP BY user_id, repo_id
    HAVING count(*) > 1;
    ```
    
    Removing these records and then applying the unique key should prevent these
    duplicates from happening in the future. If in routine usage, this unique
    constraint is broken, we will now get exceptions (that we'll need to fix)
    instead of bad data.
    
    This PR removes the `membership` rake tasks because given the new DB constraints
    it should not be possible to have bad data (that the tasks used to clean up).
    
    https://trello.com/c/rVCmKMc1/533-there-are-duplicate-memberships

---
+++ db/migrate/20150303212157_add_unique_constraint_to_memberships.rb
@@ -0,0 +1,26 @@
+class AddUniqueConstraintToMemberships < ActiveRecord::Migration
+  def self.up
+    remove_duplicate_memberships
+
+    add_index :memberships, [:user_id, :repo_id], unique: true
+    remove_index :memberships, column: :user_id
+  end
+
+  def self.down
+    remove_index :memberships, [:user_id, :repo_id]
+    add_index :memberships, :user_id
+  end
+
+  def remove_duplicate_memberships
+    sql = <<-SQL
+      DELETE FROM memberships m
+      USING memberships m2
+      WHERE
+        m.user_id = m2.user_id AND
+        m.repo_id = m2.repo_id AND
+        m.id < m2.id
+    SQL
+
+    connection.execute(sql)
+  end
+end
