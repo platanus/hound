71fc67de4e3355bd36dd3ceebc9025c3bc8dee24
srt32 <simon.taranto@gmail.com>
2015-03-03 16:51:36 -0800

    Run migration in transaction

--- db/migrate/20150303212157_add_unique_constraint_to_memberships.rb
+++ db/migrate/20150303212157_add_unique_constraint_to_memberships.rb
@@ -1,9 +1,11 @@
 class AddUniqueConstraintToMemberships < ActiveRecord::Migration
   def self.up
-    remove_duplicate_memberships
+    transaction do
+      remove_duplicate_memberships
 
-    add_index :memberships, [:user_id, :repo_id], unique: true
-    remove_index :memberships, column: :user_id
+      add_index :memberships, [:user_id, :repo_id], unique: true
+      remove_index :memberships, column: :user_id
+    end
   end
 
   def self.down
@@ -21,6 +23,6 @@ class AddUniqueConstraintToMemberships < ActiveRecord::Migration
         m.id < m2.id
     SQL
 
-    connection.execute(sql)
+    execute(sql)
   end
 end
